fbfc7f15bd05f57860542999b92c4244ccd2252a
mzawadi <mwaranguallan345@gmail.com>
2022-01-11 17:06:17 +0300

    add token and deployment config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks:{
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/WBOqt7ypLRMHCPDOvfxe0CXVbZGKzvbp",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
